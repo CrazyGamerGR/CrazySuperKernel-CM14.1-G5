c712d938f2ec1e1c20a05051c0f3b7eeca17eca2
FrancescoCG <frank-97@hotmail.gr>
2017-01-02 17:10:40 +0200

    fix build_anykernel.sh

--- build_anykernel.sh
+++ build_anykernel.sh
@@ -13,11 +13,11 @@ export CROSS_COMPILE=/home/francesco/aarch64-linux-android-4.9-kernel/bin/aarch6
 mkdir outputh830
 mkdir outputh850
 ##########################################
-make -C $(pwd) O=output cyanogenmod_h830_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=outputh830 cyanogenmod_h830_defconfig
+make -C $(pwd) O=outputh830
 
-make -C $(pwd) O=output cyanogenmod_h850_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=outputh850 cyanogenmod_h850_defconfig
+make -C $(pwd) O=outputh850
 ##########################################
 cp outputh830/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
 cp outputh850/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
